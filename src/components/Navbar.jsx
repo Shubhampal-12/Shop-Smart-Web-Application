945af59618fae969a7b31d7365bcc1397aedf51d
shubham pal <shubham.pal88799@gmail.com>
2025-07-20 15:49:09 +0530

    Add a file

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {assets} from '../assets/admin_assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-5 font-medium'>
+        <img src={assets.logo} className='w-36' alt="" />
+        u
+    </div>
+  )
+}
+
+export default Navbar
